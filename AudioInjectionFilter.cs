d996950de4ac584a73b97c666aa8a9453274afc8
jangxx <janscheiper95@gmail.com>
2022-05-29 17:09:33 +0200

    initial commit

---
+++ AudioInjectionFilter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using MelonLoader;
+using UnhollowerBaseLib;
+using NAudio.Wave;
+
+namespace LocalAudioLinkTakeover
+{
+    internal class AudioInjectionFilter : MonoBehaviour
+    {
+        private MelonLogger.Instance LoggerInstance = new MelonLogger.Instance("Local_AudioLink_Takeover::AudioInjectionFilter");
+
+        private const int SAMPLE_RATE = 48000;
+
+        private int sample = 0;
+
+        public AudioInjectionFilter(IntPtr obj0) : base(obj0)
+        {
+
+        }
+
+        public void Start()
+        {
+            LoggerInstance.Msg("AudioInjectionFilter was started");
+        }
+
+        public void OnAudioFilterRead(Il2CppStructArray<float> data, int channels)
+        {
+            //LoggerInstance.Msg("reading " + data.Length + " samples with " + channels + " channels");
+            for (int i = 0; i < data.Count; i++)
+            {
+                float x = (float)this.sample / (float)SAMPLE_RATE;
+                data[i] = (float)Math.Sin(x * 440 * Math.PI * 2) * 0.7f;
+
+                sample = (sample + 1) % SAMPLE_RATE;
+            }
+        }
+    }
+}
---
+++ AudioLinkTakeoverMain.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MelonLoader;
+using VRC;
+using NAudio.Wave;
+using UnityEngine;
+using UnhollowerRuntimeLib;
+
+namespace LocalAudioLinkTakeover
+{
+    public class AudioLinkTakeoverMain : MelonMod
+    {
+        private AudioSource emptyAudioSource = null;
+
+        public AudioLinkTakeoverMain()
+        {
+
+        }
+
+        public override void OnApplicationStart()
+        {
+            ClassInjector.RegisterTypeInIl2Cpp<AudioInjectionFilter>();
+        }
+
+        //public override void OnUpdate()
+        //{
+        //}
+
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            var udonBehaviors = UnityEngine.Object.FindObjectsOfType<VRC.Udon.UdonBehaviour>();
+
+            foreach (var udonBehavior in udonBehaviors)
+            {
+                if (udonBehavior.name == "AudioLink")
+                {
+                    LoggerInstance.Msg("Found AudioLink behavior");
+
+                    var variableNames = udonBehavior.publicVariables.VariableSymbols.Cast<Il2CppSystem.Collections.Generic.Dictionary<string, VRC.Udon.Common.Interfaces.IUdonVariable>.KeyCollection>();
+
+                    this.emptyAudioSource = udonBehavior.gameObject.AddComponent<AudioSource>();
+                    emptyAudioSource.volume = 0.001f;
+
+                    var filter = udonBehavior.gameObject.AddComponent<AudioInjectionFilter>();
+                    filter.enabled = true;
+
+                    bool success = udonBehavior.publicVariables.TrySetVariableValue<AudioSource>("audioSource", emptyAudioSource);
+
+                    LoggerInstance.Msg(success ? "set empty audio source" : "didnt work");
+                }
+            }
+        }
+    }
+}
